9d91149ab8a73363147876ae7ec2738d8bf094f6
rgl-za <hyeonjeong2829@gmail.com>
2021-07-19 23:05:02 +0900

    no message

---
+++ Stanford University CS193P Spring 2021/Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by 지현정 on 2021/07/08.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

621c06cbe7605e4aabf22f426e4078c3802790da
rgl-za <hyeonjeong2829@gmail.com>
2021-08-31 22:11:13 +0900

    TabView 강의

---
+++ SwiftUI fundamental Tutorial/Stack_apply/Stack_apply/MyView.swift
@@ -0,0 +1,35 @@
+//
+//  MyView.swift
+//  Stack_apply
+//
+//  Created by 지현정 on 2021/08/31.
+//
+
+import SwiftUI
+
+struct MyView: View{
+    
+    var title: String
+    
+    var bgColor: Color
+    
+    var body: some View{
+        
+        ZStack{
+            
+            bgColor
+                .edgesIgnoringSafeArea(.all)
+            
+            Text(title)
+                .font(.largeTitle)
+                .fontWeight(.black)
+                .foregroundColor(Color.white)
+        }
+    }
+}
+
+struct MyView_Previews: PreviewProvider{
+    static var previews: some View{
+        MyView(title: "MyView", bgColor: Color.orange)
+    }
+}

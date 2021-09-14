46c596c7f4cc387d0243204674276e94091bd464
rgl-za <hyeonjeong2829@gmail.com>
2021-09-14 16:22:27 +0900

    no message

---
+++ SwiftUI fundamental Tutorial/QRCode/QRCode/QRCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRCodeApp.swift
+//  QRCode
+//
+//  Created by 지현정 on 2021/09/14.
+//
+
+import SwiftUI
+
+@main
+struct QRCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            QRCodeScannerExampleView()
+        }
+    }
+}
